33cc488d26f2f43bbb1670ce64b007769f47312b
pyg1007 <pyg941007@hanmail.net>
2024-10-25 17:09:35 +0900

    [2023 Kakao Blind] Emoticon Init

---
+++ app/src/main/java/com/example/kotlinalgorithm/[2023 Kakao Blind] Emoticon.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinalgorithm
+
+fun main(){
+
+    val users = arrayOf(intArrayOf(40, 10000), intArrayOf(25, 10000))
+    val emoticons = intArrayOf(7000, 9000)
+
+    println(emoticonSolution(users, emoticons).contentToString())
+
+}
+
+fun emoticonSolution(users: Array<IntArray>, emoticons: IntArray): IntArray {
+    var answer: IntArray = intArrayOf()
+
+
+
+    return answer
+}
