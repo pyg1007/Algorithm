debcaa2b280849894559dbb5cb7fc3353feb3ca9
pyg1007 <pyg941007@hanmail.net>
2024-11-03 23:54:06 +0900

    [2023 Kakao Blind] Discount number of cases

--- app/src/main/java/com/example/kotlinalgorithm/[2023 Kakao Blind] Emoticon.kt
+++ app/src/main/java/com/example/kotlinalgorithm/[2023 Kakao Blind] Emoticon.kt
@@ -9,8 +9,6 @@ fun main(){
 
 }
 
-private lateinit var disCount: ArrayList<Int>
-
 /**
  *
  *  할인율은 10% 20% 30% 40% 중 하나
@@ -18,15 +16,42 @@ private lateinit var disCount: ArrayList<Int>
  */
 
 fun emoticonSolution(users: Array<IntArray>, emoticons: IntArray): IntArray {
-    var answer: IntArray = intArrayOf()
-    disCount = arrayListOf(1, 2, 3, 4)
-    val result = ArrayList<ArrayList<Int>>()
-    backTrackingDisCount(disCount.size, emoticons.size, result, arrayListOf())
-    println(result)
-    return answer
+    // 이모티콘플러스 , 이모티콘 구매비용
+    val answer = ArrayList<Pair<Int, Int>>()
+    // 할인율은 10% 20% 30% 40% 중 하나
+    val disCountRate = arrayListOf(1, 2, 3, 4)
+    // 할인율 경우의 수를 저장할 리스트
+    val disCountResult = ArrayList<ArrayList<Int>>()
+    // 경우의 수 구하기
+    backTrackingDisCount(disCountRate, disCountRate.size, emoticons.size, disCountResult, arrayListOf())
+
+    for(index in disCountResult.indices){
+        var first = 0
+        var second = 0
+        for (user in users){
+            var price = 0
+            // 물품 할인율이 유저가 설정한 할인율보다 크다면 모두 구매
+            disCountResult[index].forEachIndexed { resultIndex, discount ->
+                if (discount * 10 >= user[0]){
+                    price += emoticons[resultIndex] - (emoticons[resultIndex] * (discount * 0.1)).toInt()
+                }
+            }
+            // 이모티콘 구매비용이 설정한 금액보다 비싼경우 이모티콘 플러스에 가입
+            if (price >= user[1]){
+                first++
+            }else{
+                //아닌경우 비용 증가
+                second += price
+            }
+        }
+        answer.add(Pair(first, second))
+    }
+
+    // 이모티콘 플러스가입자가 가장많아야하고, 그이후로는 비용이 가장 커야하기 떄문에 소트를 해준 후 첫번째 값을 가져옮
+    return answer.sortedWith(compareBy<Pair<Int,Int>> { -it.first }.thenBy{ -it.second})[0].toList().toIntArray()
 }
 
-private fun backTrackingDisCount(n: Int, r: Int, result: ArrayList<ArrayList<Int>>, temp : ArrayList<Int>){
+private fun backTrackingDisCount(disCount: ArrayList<Int>, n: Int, r: Int, result: ArrayList<ArrayList<Int>>, temp : ArrayList<Int>){
 
     if (temp.size == r){
         result.add(temp.clone() as ArrayList<Int>)
@@ -34,7 +59,7 @@ private fun backTrackingDisCount(n: Int, r: Int, result: ArrayList<ArrayList<Int
     }else if(temp.size > r) return
     for (i in 0 until disCount.size){
         temp.add(disCount[i])
-        backTrackingDisCount(n, r, result, temp)
+        backTrackingDisCount(disCount, n, r, result, temp)
         temp.removeLast()
     }
 
